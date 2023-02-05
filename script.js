dd5ba234d0dec2659eecdf9dded3e8e5e9425188
Fernando Jimenez <fgjv06@gmail.com>
2023-02-05 18:42:48 -0300

    JS - Basic functionality, date added

---
+++ script.js
@@ -0,0 +1,18 @@
+// Info date
+const dateNumber = document.getElementById('dateNumber');
+const dateText = document.getElementById('dateText');
+const dateMonth = document.getElementById('dateMonth');
+const dateYear = document.getElementById('dateYear');
+
+// Tasks Container
+const tasksContainer = document.getElementById('tasksContainer');
+
+const setDate = () => {
+  const date = new Date();
+  dateNumber.textContent = date.toLocaleString('en', {day: 'numeric'});
+  dateText.textContent = date.toLocaleString('en', {weekday: 'long'});
+  dateMonth.textContent = date.toLocaleString('en', {month: 'short'});
+  dateYear.textContent = date.toLocaleString('en', {year: 'numeric'});
+}
+
+setDate();
